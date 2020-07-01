899d0fcfc1e0782d8f3eaf5f1244c6bb2f922231
dokolyski <dominikkalinowski15@wp.pl>
2020-07-01 19:08:52 +0200

    done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Table from "./components/table";
 
 const App = () => {
     return (
-        <Table />
+        <div>
+            <h4 style={{textAlign: 'center'}}>Sample images: image1.jpg, ..., image7.jpg</h4>
+            <Table />
+        </div>
     );
 }
 
